214748d48837c4b9e38487515d3f6ed47c0798ea
descellia <descelliiaa@gmail.com>
2025-09-26 11:29:36 +0700

    Commit No.2 - Clickable RecyclerView

--- app/src/main/java/com/example/lab_week_06/CatAdapter.kt
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -7,8 +7,9 @@ import com.example.lab_week_06.model.CatModel
 
 class CatAdapter(
     private val layoutInflater: LayoutInflater,
-    private val imageLoader: ImageLoader
-) : RecyclerView.Adapter<CatViewHolder.CatViewHolder>() {
+    private val imageLoader: ImageLoader,
+    private val onClickListener: OnClickListener
+) : RecyclerView.Adapter<CatViewHolder>() {
 
     private val cats = mutableListOf<CatModel>()
 
@@ -18,14 +19,18 @@ class CatAdapter(
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder.CatViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
         val view = layoutInflater.inflate(R.layout.item_list, parent, false)
-        return CatViewHolder.CatViewHolder(view, imageLoader)
+        return CatViewHolder(view, imageLoader, onClickListener)
     }
 
     override fun getItemCount() = cats.size
 
-    override fun onBindViewHolder(holder: CatViewHolder.CatViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
         holder.bindData(cats[position])
+    }
+
+    interface OnClickListener {
+        fun onItemClick(cat: CatModel)
     }
 }
--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lab_week_06
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,9 +16,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val catAdapter by lazy {
-        // Glide is used here to load the images
-        CatAdapter(layoutInflater, GlideImageLoader(this))
+        //Glide is used here to load the images
+        //Here we are passing the onClickListener function to the Adapter
+        CatAdapter(layoutInflater, GlideImageLoader(this), object:
+            CatAdapter.OnClickListener {
+            //When this is triggered, the pop up dialog will be shown
+            override fun onItemClick(cat: CatModel) = showSelectionDialog(cat)
+        })
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,5 +68,15 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+//This will create a pop up dialog when one of the items from the recycler view is clicked.
+private fun showSelectionDialog(cat: CatModel) {
+    AlertDialog.Builder(this)
+        //Set the title for the dialog
+        .setTitle("Cat Selected")
+        //Set the message for the dialog
+        .setMessage("You have selected cat ${cat.name}")
+        //Set if the OK button should be enabled
+        .setPositiveButton("OK") { _, _ -> }.show()
     }
 }
